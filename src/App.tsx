3d97534e2e1f2d3990da0d5ecb658702d453c7bf
Roman Chalyi <romanchalyi@MacBook-Pro-Roma.local>
2026-02-10 11:35:40 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello my friend2!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ minHeight: '100vh' }}>
+      <iframe
+        title="Typeform"
+        src="https://form.typeform.com/to/Q2GcxtHr"
+        style={{ width: '100%', height: '100vh', border: '0' }}
+        allow="camera; microphone; autoplay; encrypted-media;"
+      />
     </div>
   );
 }
